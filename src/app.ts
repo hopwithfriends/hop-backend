e584bee9e341bda4ea42907d2bc78054500c696f
Manuel Rosa <96manuelr@gmail.com>
2024-08-10 10:46:47 +0100

    update dev branch (#3)
    * feat(init):init (#1)
    
    * Delete .vscode directory (#2)
    
    .vscode remove

---
+++ src/app.ts
@@ -0,0 +1,27 @@
+import express from "express";
+import Sentry from "@sentry/node";
+import cors from "cors";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocument from "../swagger.json";
+const app = express();
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+	res.send("hello");
+});
+
+// ! Setup SENTRY for deployment
+/*
+import "../instrument";
+Sentry.setupExpressErrorHandler(app);
+// Optional fallthrough error handler
+app.use(function onError(err, req, res, next) {
+	res.statusCode = 500;
+	res.end(`${res.sentry} + \n`);
+});*/
+
+export default app;
