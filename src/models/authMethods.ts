877f210682e80e9af38cfaff7b9e495dc52409eb
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-12 19:44:21 +0100

    feat(auth): implement auth MVC files
    routes, controller & models implemented

---
+++ src/models/authMethods.ts
@@ -0,0 +1,23 @@
+import { db } from "../server";
+import { users } from "./schema";
+import { z } from "zod";
+
+class AuthMethods {
+	async insertUser(userId: string) {
+		try {
+			const createdUser = await db
+				.insert(users)
+				.values({
+					id: userId,
+				})
+				.returning();
+			return createdUser[0];
+		} catch (error) {
+			// Improve error handling here!
+			console.error(error);
+			throw new Error("Failed to register user!");
+		}
+	}
+}
+
+export default new AuthMethods();
