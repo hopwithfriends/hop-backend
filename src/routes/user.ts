6558934a3ab00b6192afa162aa2344dd34bc53f5
Haroon Bakhsh <hmbakhsh01@gmail.com>
2024-08-13 20:51:56 +0100

    fix: user router merge conflicts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -2,24 +2,9 @@ import express from "express";
 import userController from "../controllers/userController";
 export const userRouter = express.Router();
 
-<<<<<<< Updated upstream
-//Basic
-<<<<<<< Updated upstream
-// userRouter.get("/", userController.getAllUsers);
-userRouter.get("/", userController.getOneUser);
-// userRouter.post("/", userController.postUser);
-// userRouter.delete("/:id", userController.deleteUser);
-=======
-userRouter.get("/", userController.getAllUsers);
-userRouter.get("/:id", userController.getOneUser);
-userRouter.post("/", userController.postUser);
-userRouter.delete("/:id", userController.deleteUser);
-=======
 // Basic
 userRouter.get("/", userController.getOneUser);
-userRouter.post("/", userController.postFriendController);
->>>>>>> Stashed changes
->>>>>>> Stashed changes
+userRouter.post("/", userController.putUser);
 
 // Friend
 userRouter.post("/friend/:friendId", userController.postFriendController);
